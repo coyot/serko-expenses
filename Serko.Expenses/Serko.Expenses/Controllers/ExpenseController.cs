b3b81eb91e9113cc3c6685ee79484bb7939b5a9e
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2019-07-11 22:46:00 +0200

    GET has no point

--- Serko.Expenses/Serko.Expenses/Controllers/ExpenseController.cs
+++ Serko.Expenses/Serko.Expenses/Controllers/ExpenseController.cs
@@ -15,26 +15,10 @@ namespace Serko.Expenses.API.Controllers
     public class ExpenseController : ControllerBase
     {
         IEngine _engine;
-        ICalculator _calc;
 
         public ExpenseController(IEngine engine, ICalculator calc)
         {
             _engine = engine;
-            _calc = calc;
-        }
-
-        // GET api/expense
-        [HttpGet]
-        public ActionResult<IDictionary<string, string>> Get(string value)
-        {
-            try
-            {
-                return Ok(_engine.ParseAndCalculateGst(value));
-            }
-            catch (ExpenseException e)
-            {
-                return StatusCode(500, e.Message);
-            }
         }
 
         // POST api/expense
